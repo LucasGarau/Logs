bfeda8260ecfed68d576ba3b2cf598eb6870b39a
LucasGarau <Lucas1012000>
2019-10-16 19:04:35 +0200

    A

--- app/src/main/java/com/example/logsdeelaverno/MainActivity.java
+++ app/src/main/java/com/example/logsdeelaverno/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.logsdeelaverno;
 
 import  androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,4 +54,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    public boolean onTouchEvent (MotionEvent event){
+        int action = MotionEventCompat.getActionMasked(event);
+        switch(action){
+            case(MotionEvent.ACTION_DOWN) :
+                Log.d("yes", "Action was down");
+                return true;
+            case(MotionEvent.ACTION_MOVE) :
+                Log.d("yes", "Action was Move");
+                return true;
+            case(MotionEvent.ACTION_UP) :
+                Log.d("yes", "Action was Up");
+                return true;
+            case(MotionEvent.ACTION_CANCEL) :
+                Log.d("yes", "Action was Cancel");
+                return true;
+            case(MotionEvent.ACTION_OUTSIDE) :
+                Log.d("yes", "Movement occurred outside bounds \" +\n" +
+                        "                    \"of current screen element\"");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+
+    }
+
 }
